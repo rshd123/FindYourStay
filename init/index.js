e031f74a27fef5fda6f32d64ca07409d7043d758
M.Arshad <arshadm1512@gmail.com>
2024-11-14 09:49:22 +0530

    initializing data

--- init/index.js
+++ init/index.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const list = require("../Models/listing.js");
-
+require('dotenv').config({path:'../.env'});
+const dbURL = process.env.ATLASDB_URL;
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+  await mongoose.connect(dbURL);
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 const inDB = async ()=>{
     await list.deleteMany({});
-    initData.data = initData.data.map((el) => ({...el,owner:"6712940e2fe123c67bbe444d"}));
+    initData.data = initData.data.map((el) => ({...el,owner:"6734de04676dc8e897c1433c"}));
     await list.insertMany(initData.data);
     console.log("data was initialized");
 };
